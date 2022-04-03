27e41ca6a62f78c79ed84890a8dcb4b12a419689
GianluigiEpifani <101205771+GianluigiEpifani@users.noreply.github.com>
2022-04-03 19:29:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    float x=0, y=0, z=0, a=0, b=0;
+   cout<< "Inserire 4 numeri reali\n";
+   cin >> a >> b >> x >> y;
+   if ((x<0)&&(y>0)){
+       z=a*x-b*y;
+   }else if ((x>=0)&&(y<=0)){
+       z=a*x*x-b*y;
+   }else{
+       z=a*x+b*y*y;
+   }
+   cout << z << endl;
+   return 0;
+
+   }
